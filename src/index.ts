0d77e180972be6ff3f9000a22473fd728fd79dd4
taurus366 <taurus.ali47@gmail.com>
2025-12-25 14:18:49 +0200

    .

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 //
 // export * from './lib/xl-layout/xl-layout';
 export * from './lib/app.routes';
-// export * from './lib/component/app.floatingconfigurator';
+export * from './lib/component/app.floatingconfigurator';
 // export * from './lib/component/app.configurator';
 // export * from './lib/component/app.footer';
 // export * from './lib/component/app.layout';
